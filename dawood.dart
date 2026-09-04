bbd0181ae9e2fa12ce1a7a3b1b497349d5f4c4b1
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-04 13:51:15 -0700

    Qiestion NO:11

--- dawood.dart
+++ dawood.dart
@@ -1,6 +1,7 @@
 void main () {
  
-List information1 = ["cycle","bike","car","aeroplane","train","bike"];
-List information2 = information1.toSet().toList();
-print(information2);
+List digits = [131,42,46,88545,3,67,6,4577,535,5455343,4];
+int n = 8;
+List digits1 = digits.take(n).toList();
+print(digits1);
 }

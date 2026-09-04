781bf0d0c0ad546a948a214ab1ae03cb0fc56d93
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-04 13:28:23 -0700

    Question NO:8

--- dawood.dart
+++ dawood.dart
@@ -1,18 +1,14 @@
 void main () {
  
-Map expanses = {
-"sun": 3000.0,
-  "mon": 3000.0,
-  "tue": 3234.0
-};
+List userseligibility =[
+  {"name": "John", "eligible": true},
+{"name": "Alice", "eligible": false},
+{"name": "Mike", "eligible": true},
+{"name": "Sarah", "eligible": true},
+{"name": "Tom", "eligible": false},
+];
 
-if (expanses.containsKey("fri")){
- expanses["fri"] = 5000.0;
-}
-else {
-  print(expanses["fri"] = 5000.0);
-}
-
-print(expanses);
+userseligibility.removeWhere((userseligibility) => userseligibility["eligible"] == false);
+print (userseligibility);
 
 }

5bdedc390167b02673cc3677d258649202cf8c48
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-04 15:33:02 -0700

    Question NO:15

--- dawood.dart
+++ dawood.dart
@@ -1,9 +1,8 @@
 void main () {
  
-List numberslist1 = [21,34,54,76,87,89,01,8,32,];
+List numberslist1 = [21,-34,54,76,87,-89,01,8,-32,];
+List numberslist2 = numberslist1.where((numberslist1)=> numberslist1 > 0).toList();
 
-numberslist1.sort();
-
-print(numberslist1);
+print(numberslist2);
 
 }

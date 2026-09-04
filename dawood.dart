9e4692602d400b0d1ea5fe4b85120a200fa022a5
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-04 14:07:29 -0700

    Question NO:12

--- dawood.dart
+++ dawood.dart
@@ -1,7 +1,9 @@
 void main () {
  
-List digits = [131,42,46,88545,3,67,6,4577,535,5455343,4];
-int n = 8;
-List digits1 = digits.take(n).toList();
-print(digits1);
+List basic = ["pakistan","uzbakistan","eygpt","morroco","turkey","iran","palestine"];
+
+List basic1 = basic.reversed.toList();
+
+print(basic1);
+
 }

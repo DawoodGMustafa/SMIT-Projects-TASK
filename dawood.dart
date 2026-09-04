00d1bc0863b155bb39bda3e040a9032bb5dafa7a
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-04 15:43:35 -0700

    Question NO:16

--- dawood.dart
+++ dawood.dart
@@ -1,7 +1,8 @@
 void main () {
  
-List numberslist1 = [21,-34,54,76,87,-89,01,8,-32,];
-List numberslist2 = numberslist1.where((numberslist1)=> numberslist1 > 0).toList();
+List numberslist1 = [21,34,54,76,87,89,01,8,32,];
+
+List numberslist2 = numberslist1.where((numberslist1) => numberslist1 %2 ==0).toList();
 
 print(numberslist2);
 

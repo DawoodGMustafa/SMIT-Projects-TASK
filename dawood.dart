5d13ef3b7e1e3fa828c6eeabb01586f558474546
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-11 13:47:23 -0700

    Question NO:7

--- dawood.dart
+++ dawood.dart
@@ -1,9 +1,13 @@
 void main (){
 
-for (int i = 1; i <= 20; i++){
-    if (i % 2 == 0 )
-    print (i);
+List numbers1 = [1,2,3,4,5,6,7,8,9,10];
+
+int numbers2 = 0;
+
+for (int numbers in numbers1){
+    numbers2 += numbers;
 }
 
+print (numbers2);
 
 }

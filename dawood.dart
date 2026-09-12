ccf45ebe26d2579519463626a013a6983c1c9934
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-11 21:15:41 -0700

    Question NO:14

--- dawood.dart
+++ dawood.dart
@@ -1,21 +1,21 @@
-import 'dart:io';
+void main (){
 
-void main() {
+List numbers = [65.4,78.89,70,54.33,86.98,43.56,23.09];
 
+for (num percentage in numbers){
+if (percentage > 50){
+  print(percentage);
+}
 
-  for (int number1 = 1; number1 <= 5; number1++) {
-    print("value $number1:");
+}
 
-
-
-    int number = int.parse(stdin.readLineSync()!);
-    print("data: $number");
-  }
 
 
 
 
 }
+
+
 
 
 

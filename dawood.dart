f544d30efae819ba817c3226a719f36571201adb
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-09-11 21:21:39 -0700

    Question NO:15

--- dawood.dart
+++ dawood.dart
@@ -1,14 +1,11 @@
 void main (){
 
-List numbers = [65.4,78.89,70,54.33,86.98,43.56,23.09];
+int data =1;
 
-for (num percentage in numbers){
-if (percentage > 50){
-  print(percentage);
+while (data <= 10){
+  print (data);
+  data++;
 }
-
-}
-
 
 
 
